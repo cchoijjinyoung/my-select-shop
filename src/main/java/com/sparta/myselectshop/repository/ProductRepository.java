c75f3af511604904ca51478f7be46dd609f384f4
cchoijjinyoung <cgy7127@gmail.com>
2025-02-05 20:55:01 +0900

    폴더 별 관심상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package com.sparta.myselectshop.repository;
 
-import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
 import org.springframework.data.domain.Page;
@@ -12,4 +11,19 @@ public interface ProductRepository extends JpaRepository<com.sparta.myselectshop
   Page<com.sparta.myselectshop.entity.Product> findAllByUser(User user, Pageable pageable);
 
   Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+  /*
+   select
+     p.id,
+     p.title as product_title,
+     pf.product_id as product_id,
+     pf.folder_id as folder_id
+   from
+      product p left join product_folder pf
+      on p.id = pf.product_id
+   where p.user_id = 1
+      and
+      pf.folder_id = 3;
+   order bt p.id desc;
+      limit 10, 10 -> 10번부터 10개
+   */
 }
